e3a6883b335a7c882c4286b48521e9945d026048
herbiemolina <herbiemolina@gmail.com>
2019-03-29 10:08:42 -0400

    small changes

--- assets/logic.js
+++ assets/logic.js
@@ -21,6 +21,29 @@ $("#search").on("click", function(event) {
 
         var results = response.data;
 
-        // console.log(respons.hits[0].recipe.lable)
+        
     })
+
+    
+
+    // var query = $("#ingredient-input").val().trim();
+    // console.log(query);
+
+    // var queryURL = "https://www.themealdb.com/api/json/v1/1/search.php?s=" +
+    // query;
+
+    // $.ajax({
+    //     url: queryURL,
+    //     method: "GET"
+    // }).then(function(response) {
+    //     console.log(queryURL);
+    //     console.log(response);
+
+    //     var results = response.data;
+
+        
+    // })
+    
+
+    
 })
