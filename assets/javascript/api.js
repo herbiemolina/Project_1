29e67ff976985adee29421ec40763b81e9d9beee
Sally Beckwith <sallypbeckwith@gmail.com>
2019-03-29 18:56:17 -0400

    API call to get the recipe id number, image, and title is working

--- assets/javascript/api.js
+++ assets/javascript/api.js
@@ -1,4 +1,5 @@
-// THIS IS WORKING- but fan was going crazy on my laptop - commented it out temporarily!
+// ===== Variables =============================
+// const recipes = [];
 
 // $("#spoonacular-search").on("click", function(event) {
 //     event.preventDefault();
@@ -20,19 +21,53 @@
 //   });
 // })
 
-    
+$("#spoonacular-search").on("click", function(event) {
+  event.preventDefault();
+  var items = ["beef","cashews","onions"];
+  getRecipes(items, 3);
+})
 
-    // var query = $("#ingredient-input").val().trim();
-    // console.log(query);
+/*
+ *  Makes AJAX call to get the recipes based on the search criteria provided.
+ *
+ *  Parameters:
+ *    ingredients:   an array of ingredients to include in the recipe
+ *    quantity:      the number of recipes to return 
+ */
+const getRecipes = (ingredients, quantity) => {
+  console.log(ingredients);
+  // The next line changes the array into a string where each item is separated by %2C and any
+  //   spaces in the search ingredients are replaced by a + 
+  var convertedString = ingredients.join("%2C").replace(/ /g,"+");
 
-    // // var queryURL = ;
+  var settings = {
+    "async": true,
+    "crossDomain": true,
+    "url": `https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/findByIngredients?number=${quantity}&ranking=1&ignorePantry=false&ingredients=${convertedString}`,
+    "method": "GET",
+    "headers": {
+      "X-RapidAPI-Key": "13639e06b5msh6d9dc61e3c615b9p1f0efcjsn980ee447f232",
+      "cache-control": "no-cache",
+      "Postman-Token": "93e85d06-a15d-45b5-b778-3914b88e4fe0"
+    }
+  }
+  
+  $.ajax(settings).done(function (response) {
+    var recipes = createRecipeArray(response);
+    console.log(recipes);
+  });
+}
 
-    // $.ajax({
-    //     url: queryURL,
-    //     method: "GET"
-    // }).then(function(response) {
-    //     console.log(queryURL);
-    //     console.log(response);
+// Generates an array of recipe objects. Each object contains the recipe ID, title, and image url.
+function createRecipeArray(resp) {
+  var recipes = [];
+  resp.forEach((o) => {
+    recipes.push({
+      "id": o.id,
+      "title": o.title,
+      "image": o.image,
+    })
+  })
+  return recipes;
+}
 
-    //     var results = response.data;
-    // })
